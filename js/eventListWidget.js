050d38a224d41998ca273d8665bce5a82d6d6fa3
Jógvan Olsen <jo@jebster.net>
2017-02-20 05:34:40 +0100

    Fixed eventListWidget undefined date bug

--- js/eventListWidget.js
+++ js/eventListWidget.js
@@ -8,11 +8,11 @@ $(function(){
                 for(var i = 0; i < testb.length; i++){
                     var e = testb[i];
 
-                    e.month = displayMonth(this, moment(e.start.date).format('M'), true);
-                    e.day_number = moment(e.start.date).format('D');
-                    e.day_name = displayDay(this, moment(e.start.date).weekday(), true);
+                    e.month = displayMonth(this, moment(e.start).format('M'), true);
+                    e.day_number = moment(e.start).format('D');
+                    e.day_name = displayDay(this, moment(e.start).weekday(), true);
 
-                    e.time_interval = moment(e.start.date).format('HH:mm');
+                    e.time_interval = moment(e.start).format('HH:mm');
 
                     evs.push(e);
                 }
